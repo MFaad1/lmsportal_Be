57ec2a3015f8b68020417cd13a0d36938817ff27
faad-50 <faad.gtp@pk.see.biz>
2023-06-15 11:18:05 +0500

    adding new url

--- Routes/route.js
+++ Routes/route.js
@@ -2,7 +2,9 @@ const express = require('express')
 
 const admin = require("../Controller/controller")
 const router = express.Router()
-
+router.get('/', (req, res)=>{
+  res.send('this is response from server')
+})
 router.post('/signup',admin.signup )
 router.get('/login', admin.login)
 
